f0f551f1972d795f55661c40c1d9e004918c8191
Prince Jain <pj122333444@gmail.com>
2025-03-10 04:39:26 +0530

    websockets doubts cleared

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,41 @@
 "use client";
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
-
 export default function Home() {
+  const [message, setMessage] = useState("");
   const [socket, setSocket] = useState(null);
-
   useEffect(() => {
-    const newSocket = io("http://localhost:3000/");
-    setSocket(newSocket);
-
-    newSocket.on("connect", () => {
-      console.log("Connected:", newSocket.id);
+    const socket = io("http://localhost:3000");
+    setSocket(socket);
+    socket.on("connect", () => {
+      console.log("connected");
+      console.log(socket.id);
     });
-
-    // Cleanup: Disconnect when component unmounts
+    socket.on("all", (data) => {
+      console.log(data);
+    });
     return () => {
-      newSocket.disconnect();
-      console.log("Socket disconnected:", newSocket.id);
+      socket.disconnect();
     };
   }, []);
 
-  return <>{socket?.id} hello</>;
+  const handle = (e) => {
+    e.preventDefault();
+    socket.emit("msg", message);
+    setMessage("");
+  };
+  return (
+    <>
+      Testing
+      <form onSubmit={handle}>
+        <input
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></input>
+        <button>Send</button>
+      </form>
+    </>
+  );
 }
